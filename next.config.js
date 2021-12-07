04c5939708769be763455abb651443058ab882da
Matisse <matisse.van.steenbergen@student.howest.be>
2021-12-07 11:01:16 +0100

    exploring the middleware in subdirectoriies in production test

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /* 
 
+Dit werkt niet maar illustreert wat ik moet bereiken
+
 const withTM = require('@vercel/edge-functions-ui/transpile')()
 
 module.exports = withTM(), {
@@ -16,15 +18,22 @@ module.exports = withTM(), {
     ],
   }
 }
-
-
 */
+
+/* 
+Dit werkt maar ik heb react strict mode en url imports wel gewoon nodig...
+
+const withTM = require('@vercel/edge-functions-ui/transpile')()
+
+module.exports = withTM()
+
+*/ 
 
 
 const withTM = require('@vercel/edge-functions-ui/transpile')()
 
 
-module.exports = {
+module.exports = withTM(), {
   reactStrictMode: true,
   experimental: {
     concurrentFeatures: true,
@@ -37,5 +46,4 @@ module.exports = {
       "https://jspm.dev/",
     ],
   }
-}, withTM()
-
+}
