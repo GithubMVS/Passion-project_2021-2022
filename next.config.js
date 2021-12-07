b37354da2ce8de0ceeb718ad50e2c047da47c7f3
Matisse <matisse.van.steenbergen@student.howest.be>
2021-12-07 10:44:00 +0100

    exploring the middleware in subdirectoriies in production test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+/* 
+
 const withTM = require('@vercel/edge-functions-ui/transpile')()
 
 module.exports = withTM(), {
+  reactStrictMode: true,
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true,
+    urlImports: [
+      'https://cdn.skypack.dev',
+      "https://framer.com/m/",
+      "https://framerusercontent.com/",
+      "https://ga.jspm.io/",
+      "https://jspm.dev/",
+    ],
+  }
+}
+
+
+*/
+
+
+module.exports = {
   reactStrictMode: true,
   experimental: {
     concurrentFeatures: true,
