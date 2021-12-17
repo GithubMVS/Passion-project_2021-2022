737f3822098d869cb948bae7fe231b221600d745
Matisse <matisse.van.steenbergen@student.howest.be>
2021-12-17 03:48:42 +0100

    small bug fix

--- components/demos/server_components/goBackButton.client.js
+++ components/demos/server_components/goBackButton.client.js
@@ -10,7 +10,7 @@ const GoBackButton = () => {
   return (
     <div style={{ padding: "5rem 20rem", display: "flex", alignItems: "center" }}>
       <Image src={goBack} alt='goBack' />
-      <a href="https://passion-project-2021-2022-nv3uups0i-githubmvs.vercel.app/" style={{ color: "#9055FF", textDecoration: "none", fontFamily: "Neue Montreal, Helvetica, sans-serif, Arial", fontSize: "2rem", paddingLeft: "2.5rem", fontWeight: "700" }}>See other demos</a>
+      <a href="https://passion-project-2021-2022.vercel.app/" style={{ color: "#9055FF", textDecoration: "none", fontFamily: "Neue Montreal, Helvetica, sans-serif, Arial", fontSize: "2rem", paddingLeft: "2.5rem", fontWeight: "700" }}>See other demos</a>
     </div >
   )
 }
